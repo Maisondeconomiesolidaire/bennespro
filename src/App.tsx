9ec5363a3c6c7aacb0c25fe6f2097f2b6644078e
Selim <salem@MacBook-Pro-de-Salem.local>
2026-07-07 16:35:41 +0200

    Show update available banner

--- src/App.tsx
+++ src/App.tsx
@@ -6,38 +6,42 @@ import { Depots } from "./pages/Depots";
 import { Entreprises } from "./pages/Entreprises";
 import { Dib } from "./pages/Dib";
 import { Compte } from "./pages/Compte";
+import { UpdateAvailableBanner } from "./components/UpdateAvailableBanner";
 
 export default function App() {
   return (
-    <Routes>
-      <Route element={<AppLayout />}>
-        <Route
-          index
-          element={
-            <RequirePermission pageKey={PAGE_DEPOTS}>
-              <Depots />
-            </RequirePermission>
-          }
-        />
-        <Route
-          path="/entreprises"
-          element={
-            <RequirePermission pageKey={PAGE_ENTREPRISES}>
-              <Entreprises />
-            </RequirePermission>
-          }
-        />
-        <Route
-          path="/dib"
-          element={
-            <RequirePermission pageKey={PAGE_DEPOTS}>
-              <Dib />
-            </RequirePermission>
-          }
-        />
-        <Route path="/compte" element={<Compte />} />
-      </Route>
-      <Route path="*" element={<Navigate to="/" replace />} />
-    </Routes>
+    <>
+      <UpdateAvailableBanner />
+      <Routes>
+        <Route element={<AppLayout />}>
+          <Route
+            index
+            element={
+              <RequirePermission pageKey={PAGE_DEPOTS}>
+                <Depots />
+              </RequirePermission>
+            }
+          />
+          <Route
+            path="/entreprises"
+            element={
+              <RequirePermission pageKey={PAGE_ENTREPRISES}>
+                <Entreprises />
+              </RequirePermission>
+            }
+          />
+          <Route
+            path="/dib"
+            element={
+              <RequirePermission pageKey={PAGE_DEPOTS}>
+                <Dib />
+              </RequirePermission>
+            }
+          />
+          <Route path="/compte" element={<Compte />} />
+        </Route>
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+    </>
   );
 }
